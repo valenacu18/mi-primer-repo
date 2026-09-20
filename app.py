3703be2a9c0688bb0d47f89987bb6c82e4d9beb0
valenacu18 <val3nacu18@gmail.com>
2026-09-20 18:59:23 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+st.title("Mi Primera Aplicación con Machine Learning")
+st.write("¡Hola! Esta app está corriendo en Streamlit Cloud.")
+
+# Ejemplo de cómo cargarías tu modelo .pkl más adelante:
+# modelo = joblib.load('tu_modelo.pkl')
